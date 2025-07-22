73f09f3258f255414d52b915340ee81b94029b09
Rustamov Khumoyun <122399191+humoyuntech@users.noreply.github.com>
2025-07-22 17:34:11 +0500

    Student Page

---
+++ src/pages/Students/All/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import edit from "../../../assets/icons/edit.svg?react";
+
+const Container = styled.div``;
+
+const Action = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  z-index: 9999;
+`;
+
+Action.Edit = styled(edit)`
+  padding: 10px;
+  width: 42px;
+  height: 42px;
+`;
+Action.Move = styled(edit)`
+  padding: 10px;
+  width: 42px;
+  height: 42px;
+`;
+
+export {Container, Action}
